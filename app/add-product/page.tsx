bd22a544a6a2ba96da5cf9c1727ac75d7730896f
Ken Mwangi <kenmwang071@gmail.com>
2023-07-23 10:57:59 +0300

    reorganization

--- app/add-product/page.tsx
+++ app/add-product/page.tsx
@@ -1,9 +1,17 @@
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Add product",
+};
 
 export default function AddProduct() {
   return (
     <div>
-      <h1>Add Product</h1>
+      <h1 className="h3 mb-3">Add Product</h1>
+      <form>
+        <input type="text" className="form-input" />
+      </form>
     </div>
   );
 }
