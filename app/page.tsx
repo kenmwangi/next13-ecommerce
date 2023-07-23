223075d860788cf41b88d4f573019bb8a9f9e5fb
Ken Mwangi <kenmwang071@gmail.com>
2023-07-23 19:34:19 +0300

    products

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Container from "@/components/Container";
 import ProductCard from "@/components/ProductCard";
 import { prisma } from "@/lib/prisma";
+import Link from "next/link";
 import React from "react";
 
 export default async function Home() {
@@ -16,10 +17,13 @@ export default async function Home() {
         <div className="pb-12 md:pb-20">
           <h1 className="h1">Ecommerce Shop</h1>
         </div>
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          <div className="lg:col-span-2">
-            <ProductCard product={products[0]} />
-          </div>
+        {/* <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          
+         </div> */}
+        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {products.map((product) => {
+            return <ProductCard key={product.id} product={product} />;
+          })}
         </div>
       </Container>
     </div>
