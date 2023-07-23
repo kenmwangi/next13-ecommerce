84a60016bcdeecfe58319afd8cfb60b52ba76527
Ken Mwangi <kenmwang071@gmail.com>
2023-07-23 13:32:32 +0300

    Button component

---
+++ components/Buttons/FormSubmitButton.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React, { ComponentProps } from "react";
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
+
+type FormSubmitButtonProps = {
+  children: React.ReactNode;
+  className?: string;
+} & ComponentProps<"button">;
+
+export default function FormSubmitButton({
+  children,
+  className,
+  ...props
+}: FormSubmitButtonProps) {
+  const { pending } = useFormStatus();
+
+  return (
+    <button {...props} className={className} type="submit" disabled={pending}>
+      {pending ? <span>Loading ...</span> : ""}
+      {children}
+    </button>
+  );
+}
