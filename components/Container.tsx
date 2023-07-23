e3ad0b566d699d811f411d70b4adb11667497cce
Ken Mwangi <kenmwang071@gmail.com>
2023-07-23 11:55:18 +0300

    Container wrapper

---
+++ components/Container.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+interface ContainerProps {
+  children: React.ReactNode;
+}
+export default function Container({ children }: ContainerProps) {
+  return <div className="max-w-6xl mx-auto px-4 sm:px-6">{children}</div>;
+}
