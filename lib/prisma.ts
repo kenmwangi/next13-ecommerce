50959fe4211b920e1717639a927cf86247663d9b
Ken Mwangi <kenmwang071@gmail.com>
2023-07-23 16:21:45 +0300

    Prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-// PrismaClient is attached to the `global` object in development to prevent
-
-const globalForPrisma = global as unknown as {
+const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
