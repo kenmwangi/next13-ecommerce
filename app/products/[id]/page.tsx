12e32e19c45996b128c1075d7a4af8a13bddf613
Ken Mwangi <kenmwang071@gmail.com>
2023-07-24 09:27:04 +0300

    Single product

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,60 @@
+import { prisma } from "@/lib/prisma";
+import Container from "@/components/Container";
+import { formatPrice } from "@/lib/formatPrice";
+import { Metadata } from "next";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { cache } from "react";
+
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const getProduct = cache(async (id: string) => {
+  const product = await prisma.product.findUnique({ where: { id } });
+  if (!product) notFound();
+  return product;
+});
+
+export async function generateMetadata({
+  params: { id },
+}: ProductPageProps): Promise<Metadata> {
+  const product = await getProduct(id);
+
+  return {
+    title: product.name + " - Flowmazon",
+    description: product.description,
+    openGraph: {
+      images: [{ url: product.imageUrl }],
+    },
+  };
+}
+
+export default async function ProductPage({
+  params: { id },
+}: ProductPageProps) {
+  const product = await getProduct(id);
+
+  return (
+    <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+      <Container>
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          width={500}
+          height={500}
+          className="rounded-lg"
+          priority
+        />
+
+        <div>
+          <h1 className="text-5xl font-bold">{product.name}</h1>
+          <p className="text-gray-600">{formatPrice(product.price)}</p>
+          <p className="py-6">{product.description}</p>
+        </div>
+      </Container>
+    </div>
+  );
+}
