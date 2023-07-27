1aaeb7fb3c1756cd30234e1642d4466aa3fa17bb
Ken Mwangi <kenmwang071@gmail.com>
2023-07-27 11:48:53 +0300

    libs

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,15 +1,23 @@
 import { prisma } from "@/lib/prisma";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
+// import Adapter from "next-auth/adapters";
 import { env } from "./validations/env";
 
 import GoogleProvider from "next-auth/providers/google";
+
 export const authOptions: NextAuthOptions = {
-  // adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
 };
