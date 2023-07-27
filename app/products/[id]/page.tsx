fea1d8a9adcc572e5573f80e9f305b0e995532a2
Ken Mwangi <kenmwang071@gmail.com>
2023-07-27 08:40:07 +0300

    updated Schema

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -26,7 +26,7 @@ export async function generateMetadata({
   const product = await getProduct(id);
 
   return {
-    title: product.name + " - Flowmazon",
+    title: product.name,
     description: product.description,
     openGraph: {
       images: [{ url: product.imageUrl }],
